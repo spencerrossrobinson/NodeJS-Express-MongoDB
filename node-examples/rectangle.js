d5398e6104cc71e3e13a07af01dd484246d16de6
Spencer Robinson <spencerwarmouth@gmail.com>
2023-02-13 18:00:50 -0500

    New Backend Repo

---
+++ node-examples/rectangle.js
@@ -0,0 +1,17 @@
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    callback(
+      new Error(
+        `Rectangle Dimensions must be greater than zero. Recieved:  ${x}, ${y}`
+      )
+    );
+  } else {
+    setTimeout(() => {
+      callback(null, {
+        perimeter: () => 2 * (x + y),
+        area: () => x * y,
+      }),
+        2000;
+    });
+  }
+};
