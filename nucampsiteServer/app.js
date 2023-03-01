70b7917722dd4d72b650db6db71a33bfeed159ac
Spencer Robinson <spencerwarmouth@gmail.com>
2023-03-01 13:13:20 -0500

    Express Sessions

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
+//the require function is returning another function as return value, we immediately call that return function with the second argument session
+const FileStore = require("session-file-store")(session);
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -38,11 +41,26 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+// app.use(cookieParser("12345-67890-09876-54321"));
+
+app.use(
+  session({
+    name: "session-id",
+    //same as cookie parser
+    secret: "12345-67890-09876-54321",
+    //wont get saved and no cookie will get sent to the client
+    saveUninitialized: false,
+    //once a session is created and updated, it will continue to be ressaved even if there is no updates
+    resave: false,
+    // new filestore as an object to save new session info
+    store: new FileStore(),
+  })
+);
 
 function auth(req, res, next) {
+  console.log(req.session);
   //signedCookies from cookie parser, will automatically parse signed cookie from req, if not properly signed will return false, user comes from us
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       //if authHeader is null returning a error message to let the client know they are not authenticated
@@ -63,7 +81,7 @@ function auth(req, res, next) {
     const pass = auth[1];
     if (user === "admin" && pass === "password") {
       //express knows to use secret cookie from cookie parser  to sign cookie
-      res.cookie("user", "admin", { signed: true });
+      req.session.user = "admin";
       return next(); //authorized
     } else {
       //if not authorized returns error and trys authorization again
@@ -74,7 +92,7 @@ function auth(req, res, next) {
     }
   } else {
     //checks for signed cookie
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "admin") {
       return next();
     } else {
       // returns error if cookie is not signed and prompts for login
